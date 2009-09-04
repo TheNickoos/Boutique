d22716b3e6880a15e907c8649cdb75b2d990f8e5
Nickoos <nickoos666@hotmail.com>
2009-09-04 17:58:42 +0200

    Ajout du patch + mise a jour du SQL

--- sql/mangos_boutique.sql
+++ sql/mangos_boutique.sql
@@ -4,46 +4,39 @@
 -- ------------------------------------------------------
 -- Server version	5.0.51a-24+lenny2
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE TABLE IF NOT EXISTS `boutique_money` (
+  `gain` int(11) NOT NULL default '10000',
+  `prix` int(11) NOT NULL default '1',
+  `level_min` int(11) NOT NULL default '1',
+  `level_max` int(11) NOT NULL default '80'
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
---
--- Table structure for table `boutique_item`
---
 
-DROP TABLE IF EXISTS `boutique_item`;
-SET @saved_cs_client     = @@character_set_client;
-SET character_set_client = utf8;
-CREATE TABLE `boutique_item` (
-  `itemid` int(11) NOT NULL,
-  `count` int(11) NOT NULL,
-  `prix` int(11) NOT NULL,
-  `commande` int(11) NOT NULL,
-  `category` int(11) NOT NULL default '1',
+INSERT INTO `boutique_money` (`gain`, `prix`, `level_min`, `level_max`) VALUES
+(10000, 4000, 1, 80);
+
+
+CREATE TABLE IF NOT EXISTS `boutique_level` (
+  `gain` bigint(11) NOT NULL default '1',
+  `prix` bigint(11) NOT NULL default '1',
+  `level_min` bigint(11) NOT NULL default '1',
+  `level_max` bigint(11) NOT NULL default '80'
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+
+INSERT INTO `boutique_level` (`gain`, `prix`, `level_min`, `level_max`) VALUES
+(1, 1, 1, 80);
+
+
+CREATE TABLE IF NOT EXISTS `boutique_item` (
+  `itemid` int(11) NOT NULL default '0',
+  `count` int(11) NOT NULL default '1',
+  `prix` int(11) NOT NULL default '1',
   `qte_vendue` bigint(20) unsigned NOT NULL default '0',
   `qte_max` bigint(20) unsigned NOT NULL default '0'
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
-SET character_set_client = @saved_cs_client;
 
---
--- Table structure for table `boutique_money`
---
 
-DROP TABLE IF EXISTS `boutique_money`;
-SET @saved_cs_client     = @@character_set_client;
-SET character_set_client = utf8;
-CREATE TABLE `boutique_money` (
-  `gain` int(11) NOT NULL,
-  `prix` int(11) NOT NULL,
-  `level_min` int(11) NOT NULL,
-  `level_max` int(11) NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
-SET character_set_client = @saved_cs_client;
+INSERT INTO `boutique_item` (`itemid`, `count`, `prix`, `qte_vendue`, `qte_max`) VALUES
+(30360, 1, 190, 0, 0);
+
